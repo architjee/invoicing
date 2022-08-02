71fdc95fd2af994922e16c9a5ddf8d389e1e7c18
Archit Jain <architjee@gmail.com>
2022-08-02 17:14:51 +0530

    Almost to the finish line

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
 <script>
+
+let TAXATION_VALUE = 0.03;
 export default {
   data() {
+
+    let emptyBillArray = []
+    for (let i = 0; i < 8; i++) {
+      emptyBillArray.push({ "wght": 0, "rate": 0 })
+    }
+
+
+    let remarksAmountObject = { "remarks": "", "amount": 0 };
     return {
-      invoiceObject: { "bill": [{ "wght": 0, "rate": 0 }, { "wght": 0, "rate": 0 }, { "wght": 0, "rate": 0 }, { "wght": 0, "rate": 0 }, { "wght": 0, "rate": 0 }, { "wght": 0, "rate": 0 }, { "wght": 0, "rate": 0 }, { "wght": 0, "rate": 0 }], isTaxable: false, smallTotal:0 }
+      invoiceObject: {
+        "bill": emptyBillArray, "goldSilverReturn": structuredClone(remarksAmountObject), "makingCharges": structuredClone(remarksAmountObject), "discount": structuredClone(remarksAmountObject),
+        "smallTotal": 0,
+        "isTaxable": false, "sgstTax": 0, "cgstTax": 0,
+        "bigTotal": 0,
+        "inWords": "",
+        "cashPaid": structuredClone(remarksAmountObject),
+        "chequeAmount": structuredClone(remarksAmountObject),
+        "onlineTransaction": structuredClone(remarksAmountObject),
+        "otherPaymentsObject": structuredClone(remarksAmountObject),
+        "totalPaid": 0,
+        "amountDue": 0
+
+      }
 
     }
   },
@@ -10,18 +33,73 @@ export default {
     smallTotalProp() {
       let sTotal = 0;
       this.invoiceObject.bill.forEach(billement => {
-        billement.total = billement.wght * billement.rate
+        billement.total = billement.wght * billement.rate;
         sTotal += billement.total;
       });
+      sTotal = Number(sTotal.toFixed(2))
+      sTotal = sTotal - this.invoiceObject.goldSilverReturn.amount + this.invoiceObject.makingCharges.amount + this.invoiceObject.discount.amount
       this.invoiceObject.smallTotal = sTotal;
       return this.invoiceObject.smallTotal;
+    },
+    sgstTaxCalculation() {
+      if (this.invoiceObject.isTaxable) {
+        this.invoiceObject.sgstTax = this.invoiceObject.smallTotal * (TAXATION_VALUE);
+      } else {
+        this.invoiceObject.sgstTax = 0
+      }
+      this.invoiceObject.sgstTax = Number(this.invoiceObject.sgstTax.toFixed(2));
+      return this.invoiceObject.sgstTax;
+    },
+    cgstTaxCalculation() {
+      if (this.invoiceObject.isTaxable) {
+        this.invoiceObject.cgstTax = this.invoiceObject.smallTotal * (TAXATION_VALUE);
+
+      } else {
+        this.invoiceObject.cgstTax = 0
+      }
+      this.invoiceObject.cgstTax = Number(this.invoiceObject.cgstTax.toFixed(2));
+      return this.invoiceObject.cgstTax;
+    },
+    bigTotalCalculation() {
+      let tempBigTotal = 0
+      if (this.invoiceObject.isTaxable) {
+        tempBigTotal = this.invoiceObject.smallTotal + this.invoiceObject.cgstTax + this.invoiceObject.sgstTax;
+      } else {
+        tempBigTotal = this.invoiceObject.smallTotal;
+      }
+      this.invoiceObject.bigTotal = Number(tempBigTotal.toFixed(0));
+      this.invoiceObject.inWords = this.num2Text(this.invoiceObject.bigTotal);
+      return this.invoiceObject.bigTotal;
     }
 
+
+
   },
-  methods:{
-    complexComputation(){
-      console.log(this.invoiceObject);
+  methods: {
+    complexComputation() {
+      this.smallTotal = this.smallTotalProp;
+      this.sgstTax = this.sgstTaxCalculation;
+      this.cgstTax = this.cgstTaxCalculation;
+      this.bigTotal = this.bigTotalCalculation;
+    },
+
+    num2Text(num) {
+
+      let a = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
+      let b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+      if ((num = num.toString()).length > 9) return 'overflow';
+      let n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+      if (!n) return;
+      var str = '';
+      str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
+      str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
+      str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
+      str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
+      str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + 'only ' : '';
+      return str;
     }
+
+
   }
 }
 </script>
@@ -33,7 +111,7 @@ export default {
         i-Manas
       </div>
       <div class="subtitle is-3 has-text-white">
-        Made with ♥ by AJ {{ smallTotalProp }}
+        Made with ♥ by AJ
       </div>
     </div>
     <div class="column pink is-10 has-text-centered mainForm">
@@ -147,11 +225,12 @@ export default {
           </div>
 
           <div class="field">
-            <label class="label">Message</label>
+            <label class="label">Comments/Message</label>
             <div class="control">
-              <textarea class="textarea" placeholder="Textarea"></textarea>
+              <input class="input" type="text" placeholder="Comments/Message" v-model="invoiceObject.mobileNo">
             </div>
           </div>
+
 
         </div>
 
@@ -176,27 +255,93 @@ export default {
         </tr>
 
         <tr v-for="i in invoiceObject.bill">
-          <th>
+          <td>
             <input class="input is-small" type="text" placeholder="Description of Goods" v-model="i.desc">
-          </th>
-          <th>
+          </td>
+          <td>
             <input class="input is-small" type="text" placeholder="HSN Code" v-model="i.code">
-          </th>
-          <th>
-            <input class="input is-small" type="text" placeholder="Weight" v-model.number="i.wght">
-          </th>
-          <th>
-            <input class="input is-small" type="text" placeholder="Rate" v-model.number="i.rate">
-          </th>
-          <th>
+          </td>
+          <td>
+            <input class="input is-small" type="number" placeholder="Weight" v-model.number="i.wght">
+          </td>
+          <td>
+            <input class="input is-small" type="number" placeholder="Rate" v-model.number="i.rate">
+          </td>
+          <td>
             {{ i.wght * i.rate }}
-          </th>
+          </td>
         </tr>
 
       </table>
+
+
+
+      <table class="table is-fullwidth is-striped is-bordered is-hoverable is-narrow">
+        <tr>
+          <th>
+            Particulars
+          </th>
+          <th colspan="3">
+            Remarks
+          </th>
+          <th>
+            Amount
+          </th>
+        </tr>
+        <tr>
+          <td>
+            Gold/Silver Return
+          </td>
+          <td colspan="3">
+            <input class="input is-small" type="text" placeholder="Gold/Silver Return Description"
+              v-model="invoiceObject.goldSilverReturn.remarks">
+          </td>
+          <td>
+            <input class="input is-small" type="number" placeholder="Gold/Silver Return Amount"
+              v-model.number="invoiceObject.goldSilverReturn.amount">
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Making Charges
+          </td>
+          <td colspan="3">
+            <input class="input is-small" type="text" placeholder="Making Charges Description"
+              v-model="invoiceObject.makingCharges.remarks">
+          </td>
+          <td>
+            <input class="input is-small" type="number" placeholder="Making Charges Amount"
+              v-model.number="invoiceObject.makingCharges.amount">
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Discount
+          </td>
+          <td colspan="3">
+            <input class="input is-small" type="text" placeholder="Discount Description"
+              v-model="invoiceObject.discount.remarks">
+          </td>
+          <td>
+            <input class="input is-small" type="number" placeholder="Discount Amount"
+              v-model.number="invoiceObject.discount.amount">
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Total
+          </td>
+          <td colspan="3">
+          </td>
+          <td>
+            {{ smallTotalProp }}
+          </td>
+        </tr>
+      </table>
+
       <div class="control">
         <label class="radio">
-          <input type="radio" name="answer" :value="true" v-model="invoiceObject.isTaxable" >
+          <input type="radio" name="answer" :value="true" v-model="invoiceObject.isTaxable">
           Yes
         </label>
         <label class="radio">
@@ -204,6 +349,48 @@ export default {
           No
         </label>
       </div>
+
+      <table class="table is-fullwidth is-striped is-bordered is-hoverable is-narrow">
+
+        <tr v-if="invoiceObject.isTaxable">
+          <td>
+            SGST Tax
+          </td>
+          <td colspan="3">
+          </td>
+          <td>
+            {{ sgstTaxCalculation }}
+          </td>
+        </tr>
+        <tr v-if="invoiceObject.isTaxable">
+          <td>
+            CGST Tax
+          </td>
+          <td colspan="3">
+
+          </td>
+          <td>
+            {{ cgstTaxCalculation }}
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            Grand Total
+          </td>
+          <td colspan="3">
+          </td>
+          <td>
+            {{ bigTotalCalculation }}
+          </td>
+        </tr>
+      </table>
+
+<div>
+        Total(in words) : {{ this.invoiceObject.inWords }}
+      </div>
+
+
       <div class="field is-grouped">
         <div class="control">
           <button class="button is-link">Submit</button>
